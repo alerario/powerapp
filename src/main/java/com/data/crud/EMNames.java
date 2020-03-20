c0719d095c137e7fce7b37c865c88c53a148b169
Alexandre L'Erario <alerario@gmail.com>
2020-03-20 16:05:32 -0300

    Configuracao do BD

--- src/main/java/com/data/crud/EMNames.java
+++ src/main/java/com/data/crud/EMNames.java
@@ -20,14 +20,15 @@ public class EMNames {
         String external_db = System.getenv("JDBC_DATABASE_URL"); // variavel de ambiente criada no ambiente externo
         Map<String, String> properties = new HashMap<>();
         
-        if (external_db == null) { //caso nao tenhamos a variavel de ambiente       
+        if (external_db == null) { //caso nao tenhamos a variavel de ambiente 
+            System.out.println("### Acesso local ao banco de dados  ####");
             properties.put("javax.persistence.jdbc.url", "jdbc:postgresql://localhost:5432/teste");
             properties.put("javax.persistence.jdbc.user", "postgres");
             properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
             properties.put("javax.persistence.jdbc.password", "postgres");
             properties.put("javax.persistence.schema-generation.database.action", "create");
         } else { //se a variavel de ambiente foi criada, indica que o projeto está alocado na producao
-           
+            System.out.println("### Carregando dados de acesso ao banco ####");
             String jdbc_database_url = System.getenv("JDBC_DATABASE_URL");
             String jdbc_database_username = System.getenv("JDBC_DATABASE_USERNAME");
             String jdbc_database_password = System.getenv("JDBC_DATABASE_PASSWORD");
